89dfb053076bc83293b80c73ceefea25c3e84f27
LiYan <lidoyan@gmail.com>
2018-08-10 10:18:19 +0800

    animation & transition

--- webworker/main.js
+++ webworker/main.js
@@ -13,12 +13,20 @@ const resultTwo = document.getElementById('result-two')
 const buttonOne = document.getElementById('button-one')
 const buttonTwo = document.getElementById('button-two')
 
+const titleOne = document.querySelector('.demo.left .title')
+const titleTwo = document.querySelector('.demo.right .title')
+
 buttonOne.onclick = (e) => {
-    let number = Number(inputOne.value)
-    resultOne.innerText = fib(number)
+    titleOne.classList.add('calculating')
+    setTimeout(() => {
+        let number = Number(inputOne.value)
+        resultOne.innerText = fib(number)
+        titleOne.classList.remove('calculating')
+    }, 1000)
 }
 
 buttonTwo.onclick = (e) => {
+    titleTwo.classList.add('calculating')
     worker.postMessage(Number(inputTwo.value))
 }
 
@@ -28,4 +36,5 @@ let worker = new Worker('worker.js')
 
 worker.onmessage = (e) => {
     resultTwo.innerText = e.data
+    titleTwo.classList.remove('calculating')
 }
